767408ab0ef457881473df7cc711385068aee019
kksswkd <kksswkd@gmail.com>
2022-01-24 17:22:53 +0900

    shortest path examples

---
+++ examples/shortest-path/9-1.py
@@ -0,0 +1,58 @@
+# 간단한 다익스트라 알고리즘
+import sys
+input=sys.stdin.readline
+INF=int(1e9)
+
+# 노드, 간선의 개수
+n,m=map(int,input().split())
+# 시작 노드 번호
+start=int(input())
+# 각 노드에 연결된 노드의 정보를 담는 리스트
+graph=[[] for i in range(n+1)]
+# 방문 기록
+visited = [False]*(n+1)
+# 최단 거리 테이블 무한으로 초기화
+distance=[INF]*(n+1)
+
+# 그래프 생성
+for _ in range(m):
+    a,b,c=map(int, input().split())
+    #a->b 비용 = c
+    graph[a].append((b,c))
+
+# 최단 거리가 가장 짧은 노드 찾기
+def get_smallet_node():
+    min_value=INF
+    index=0 # 가장 최단 거리가 짧은 노드 인덱스
+    for i in range(1,n+1):
+        if distance[i]<min_value and not visited[i]:
+            min_value=distance[i]
+            index=i
+    return index
+
+# 다익스트라 알고리즘
+def dijkstra(start):
+    # 시작 노드에 대해서 초기화
+    distance[start]=0
+    visited[start]=True
+    for j in graph[start]:
+        distance[j[0]]=j[1]
+    # 시작 노드를 제외한 전체 n-1개의 노드에 대해 반복
+    for i in range(n-1):
+        # 현재 최단 거리가 가장 짧은 노드를 꺼내서 방문 처리
+        now=get_smallet_node()
+        visited[now]=True
+        # 현재 노드와 연결된 다른 노드를 확인
+        for j in graph[now]:
+            cost=distance[now]+j[1]
+            # 현재 노드를 거쳐서 다른 노드로 이동하는 거리가 더 잛은 경우
+            if cost<distance[j[0]]:
+                distance[j[0]]=cost
+
+dijkstra(start)
+
+for i in range(1,n+1):
+    if distance[i]==INF:
+        print("INFINITY")
+    else:
+        print(distance[i])
